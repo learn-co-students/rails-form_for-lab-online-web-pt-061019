c558f0a13c2c2938e467d766dc387d98c7387eaf
github username <github email address>
2019-10-27 11:14:45 -0600

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,35 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(students_params)
+    redirect_to student_path(@student)
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(students_params)
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+  end
+
+
+    private
+
+    def students_params
+      params.require(:student).permit(:first_name, :last_name)
+    end
